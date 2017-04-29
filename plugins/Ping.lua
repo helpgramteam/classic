bc8d70b59a4ee20dafef2eb5e7a76ebd631036e0
helpgramteam <amir1ali2robat@gmail.com>
2017-04-29 21:28:11 +0430

    amir

--- plugins/Ping.lua
+++ plugins/Ping.lua
@@ -1,16 +1,19 @@
-﻿--Begin Ping.lua By 
-local datebase = {
-   "pong❤️",
-  }
-local function run(msg, matches) 
-return datebase[math.random(#datebase)]
+﻿--Start
+local function secure(msg, matches)
+  if matches[1] == "ping" or "انلاینی" then
+    if is_sudo(msg) then
+    tdcli.sendDocument(msg.chat_id_, 0, 0, 1, nil, './secure/ping.gif.mp4', '', dl_cb, nil)
+      return "@helpgramteam"
+    end
+  end
 end
+
 return {
   patterns = {
-    "^([Pp][Ii][Nn][Gg])",
-	"^[!/#]([Pp][Ii][Nn][Gg])",
-	"^(انلاینی)"
-  },
-  run = run
+    "^(ping)$",
+	"^(انلاینی)$"
+    },
+  run = secure
 }
---End Ping.lua--
+
+--By amir helpgramteam
