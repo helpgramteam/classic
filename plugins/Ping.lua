23903ba43f3b29b5b15c78f92431bf059934c775
helpgramteam <amir1ali2robat@gmail.com>
2017-05-13 15:03:29 +0430

    amir

--- plugins/Ping.lua
+++ plugins/Ping.lua
@@ -4,7 +4,6 @@ local function amir(msg, matches)
   if matches[1] == "ping" or "انلاینی" then
     if is_sudo(msg) then
     tdcli.sendDocument(msg.chat_id_, 0, 0, 1, nil, './amir/ping.gif.mp4', '', dl_cb, nil)
-      return "@helpgramteam"
 	elseif is_admin(msg) then
     tdcli.sendDocument(msg.chat_id_, 0, 0, 1, nil, './amir/pping.jpg', '', dl_cb, nil)
       return "@helpgramteam"
